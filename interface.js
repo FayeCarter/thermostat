89aaae31d1c7a2784fd0e6696c2352cbc51c827f
Harry Turnbull <hturnbull@live.co.uk>
2020-04-17 16:41:50 +0100

    more stuff done

--- interface.js
+++ interface.js
@@ -45,7 +45,30 @@ $(document).ready(function() {
 
   function updateTemperature() {
     $('#temperature').text(thermostat.getTemperature());
+    $('#mercury').css('height', `${mercury()}%`)
     $('#right').attr('class', thermostat.getUsage());
   }
 
+  function mercury() {
+    return thermostat.getTemperature() * 2.63 -8.7
+  }
+
 });
+
+/*
+
+32/32*100 = 100
+
+10/32*100 =  31.25
+
+30/30*100*3/4 = 75
+
+10/32*100 =  31.25
+
+
+
+
+32 =~ 75 (2.34)
+10 =~ 17 (1.7)
+
+*/
